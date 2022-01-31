f7b2e1eec2119a96040fddde945d3f9f80732620
SwapnilManohar <71309232+SwapnilManohar@users.noreply.github.com>
2022-01-31 13:27:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@
    vpc_id =  aws_vpc.Main.id
    cidr_block = "${var.public_subnets}"        # CIDR block of public subnets
  }
- Create a Private Subnet                   # Creating Private Subnets
+                                                 # Creating Private Subnets
  resource "aws_subnet" "privatesubnets" {
    vpc_id =  aws_vpc.Main.id
    cidr_block = "${var.private_subnets}"          # CIDR block of private subnets
